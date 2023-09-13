a52b4c5a2c89af1115b8112e371b15f97397fe05
We-Bus <135043751+We-Bus@users.noreply.github.com>
2023-09-13 16:25:09 +0200

    Played around in bevy, player can move from left and right to bounds and camera will follow

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,151 @@
-use bevy::{prelude::*, transform};
+use bevy::{math::Vec3Swizzles, prelude::*};
+
+const TIME_STEP: f32 = 1.0 / 60.0;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins
-            .set(ImagePlugin::default_nearest())
-            .set(WindowPlugin {
-                primary_window: Some(Window { 
-                    title: "My Game".into(),
-                    resolution: (800.0, 600.0).into(),
-                    resizable: false,
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Insert Game Name".into(),
+                    resolution: (800., 600.).into(),
+                    // Tells wasm to resize the window according to the available canvas
+                    fit_canvas_to_parent: true,
+                    // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                    prevent_default_event_handling: false,
                     ..default()
                 }),
                 ..default()
-            })
-            .build(),
-        )
+            }),
+        ))
+        .insert_resource(FixedTime::new_from_secs(TIME_STEP))
         .add_systems(Startup, setup)
-        .add_systems(Update, character_movement)
+        .add_systems(
+            FixedUpdate,
+            (
+                player_movement_system,
+                move_camera_system,
+            ),
+        )
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-
-    let texture = asset_server.load("char.png");
-
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0,100.0)),
-            ..default()
-        },
-        texture,
-        ..default()
-    });
+/// player component
+#[derive(Component)]
+struct Player {
+    /// linear speed in meters per second
+    movement_speed: f32,
 }
 
-fn character_movement(
-    mut characters: Query<(&mut Transform, &Sprite)>,
-    input: Res<Input<KeyCode>>,
-    time: Res<Time>,
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let player_texture = asset_server.load("player_idle.png");
+    let default_tile_texture = asset_server.load("tiles/plaintile.png");
+
+    // 2D orthographic camera
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        SpriteBundle {
+            texture: player_texture,
+            ..default()
+        },
+        Player {
+            movement_speed: 500.0,// meters per second
+        },
+    ));
+
+    // tile
+    commands.spawn((
+        SpriteBundle {
+            texture: default_tile_texture,
+            ..default()
+        },
+    ));
+}
+
+fn player_movement_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Player, &mut Transform)>,
 ) {
-    for (mut transform, _) in &mut characters {
-        if (input.pressed(KeyCode::W)) {
-            transform.translation.y += 100.0 * time.delta_seconds();
-        }
-        if (input.pressed(KeyCode::S)) {
-            transform.translation.y -= 100.0 * time.delta_seconds();
-        }
-        if (input.pressed(KeyCode::A)) {
-            transform.translation.x -= 100.0 * time.delta_seconds();
-        }
-        if (input.pressed(KeyCode::D)) {
-            transform.translation.x += 100.0 * time.delta_seconds();
-        }
+    let (player, mut transform) = query.single_mut();
+
+    let mut player_input : Vec3 = Vec3::ZERO;
+
+    if keyboard_input.pressed(KeyCode::W) {
+        player_input.y += 1.0;
     }
-} 
+    if keyboard_input.pressed(KeyCode::S) {
+        player_input.y += -1.0;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        player_input.x += -1.0;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        player_input.x += 1.0;
+    }
+
+    if (player_input.length_squared() > 0.001) {
+        player_input = player_input.normalize();
+    }
+
+    let movement_distance = player.movement_speed * TIME_STEP;
+    let translation_delta = player_input * movement_distance;
+    transform.translation += translation_delta;
+}
+
+fn move_camera_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut camera_query: Query<(&Camera2d, &mut Transform),Without<Player>>,
+    player_query: Query<(&Player,&Transform)>,
+) {
+    let (_, mut transform) = camera_query.single_mut();
+    let (_,player_transform) = player_query.single();
+
+    let player_x = player_transform.translation.x;
+    let player_y = player_transform.translation.y;
+
+    let camera_x = transform.translation.x;
+    let camera_y = transform.translation.y;
+
+    let max_camera_bounds = camera_x + (800./2.) - 50.;
+    let min_camera_bounds = camera_x - (800./2.) + 50.;
+    
+    let translation = &mut transform.translation;
+
+    let mut direction = Vec3::ZERO;
+    
+    if (player_x > max_camera_bounds) {
+        let delta = player_x - max_camera_bounds;
+        direction.x = delta;
+        *translation += direction * 100.0 * TIME_STEP; // Adjust the speed by multiplying with a time step
+    }
+
+    if (player_x < min_camera_bounds) {
+        let delta = player_x - min_camera_bounds;
+        direction.x = delta;
+        *translation += direction * 100.0 * TIME_STEP; // Adjust the speed by multiplying with a time step
+    }
+
+
+    //println!("{}",player_x)
+
+    // // Use the arrow keys to move the camera along the x and y axes
+    // let mut direction = Vec3::ZERO;
+    // if keyboard_input.pressed(KeyCode::Left) {
+    //     direction.x -= 1.0;
+    // }
+    // if keyboard_input.pressed(KeyCode::Right) {
+    //     direction.x += 1.0;
+    // }
+    // if keyboard_input.pressed(KeyCode::Up) {
+    //     direction.y += 1.0;
+    // }
+    // if keyboard_input.pressed(KeyCode::Down) {
+    //     direction.y -= 1.0;
+    // }
+
+    // let translation = &mut transform.translation;
+    // *translation += direction * 100.0 * TIME_STEP; // Adjust the speed by multiplying with a time step
+}
+
