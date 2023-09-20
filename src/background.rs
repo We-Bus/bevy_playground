788da6cc0ce225790ef6fd64d58e30498b67fa28
We-Bus <135043751+We-Bus@users.noreply.github.com>
2023-09-20 13:48:09 +0200

    working on stuff

--- src/background.rs
+++ src/background.rs
@@ -1,14 +1,11 @@
 use crate::prelude::*;
-use std::{thread, time};
 
 pub struct BackgroundPlugin;
 
 impl Plugin for BackgroundPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, initialize_background)
-        .add_systems(Update, (
-                update_background,
-            )
+            .add_systems(Update, (update_background,)
         );
     }
 }
@@ -16,29 +13,47 @@ impl Plugin for BackgroundPlugin {
 pub fn initialize_background(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    textures: Res<Assets<Image>>
 ) {
-    commands.spawn((
-        BackgroundRenderer {
-            loaded_sprites: Vec::new(),
-            current_center_chunk: IVec2::new(0,0),
-            has_tiles: false,
-        },
-    ));
+    let background_image_handle: Handle<Image> = asset_server.load("default_background.png");
 
-    let background_image_handle: Handle<Image> = asset_server.load("tiles/tile.png");
+    for x in -1..2 {
+        for y in -1..2 {
+            let chunk_position = chunk_index_to_chunk_position(x,y); 
+            commands.spawn((
+                SpriteBundle {
+                    texture: background_image_handle.clone(),
+                    transform: Transform::from_translation(Vec3::new(chunk_position.x,chunk_position.y,0.)),
+                    sprite: Sprite {
+                        anchor: bevy::sprite::Anchor::BottomLeft,
+                        ..default()
+                    },
+                    ..default()
+                },
+                BackgroundChunk {},
+            ));
+        }
+    }
 
-
-    commands.spawn((
-        SpriteBundle {
-            texture: background_image_handle,
-            ..default()
-        },
-    ));
-    
 }
 
 pub fn update_background(
+    mut commands: Commands,
+    camera_query: Query<&Transform,(With<Camera2d>,Without<BackgroundChunk>)>,
+    mut background_query: Query<&Transform,With<BackgroundChunk>>
 ) {
+    for bg_transfrom in background_query.iter() {
+        
+    }
+}
 
+fn position_to_chunk(x_position: f32, y_position: f32) -> IVec2 {
+    let chunk_x = (x_position / SCREEN_WIDTH as f32).floor() as i32;
+    let chunk_y = (y_position / SCREEN_HEIGHT as f32).floor() as i32;
+    IVec2 {x:chunk_x,y:chunk_y}
+}
+
+fn chunk_index_to_chunk_position(x_chunk: i32, y_chunk: i32) -> Vec2 {
+    let chunk_pos_x = x_chunk as f32 * SCREEN_WIDTH;
+    let chunk_pos_y = y_chunk as f32 * SCREEN_HEIGHT;
+    Vec2 {x:chunk_pos_x,y:chunk_pos_y}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,6 @@ pub mod prelude {
     pub const TIME_STEP: f32 = 1.0 / 60.0;
     pub const SCREEN_WIDTH: f32 = 1800.;
     pub const SCREEN_HEIGHT: f32 = 900.;
-    pub const TILE_SIZE: i32 = 32;
-    pub const CHUNK_TILE_COUNT_WIDTH: i32 = 10;
-    pub const CHUNK_TILE_COUNT_HEIGHT: i32 = 10;
-    pub const CHUNK_WIDTH: i32 = TILE_SIZE * CHUNK_TILE_COUNT_WIDTH;
-    pub const CHUNK_HEIGHT: i32 = TILE_SIZE * CHUNK_TILE_COUNT_HEIGHT;
 
     pub use crate::player::PlayerPlugin;
     pub use crate::background::BackgroundPlugin;
@@ -23,9 +18,6 @@ pub mod prelude {
     }
 
     #[derive(Component)]
-    pub struct BackgroundRenderer {
-        pub loaded_sprites: Vec<Entity>,
-        pub current_center_chunk: IVec2,
-        pub has_tiles: bool,
+    pub struct BackgroundChunk {
     }
 }  
